113276b040c2d05bcc2e3257ed3baca2bd3b6c44
gauravpatil2426-star <gauravpatil2426@gmail.com>
2026-05-13 12:09:43 +0530

    responsive

--- src/componets/MainHeader.js
+++ src/componets/MainHeader.js
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../img/logo.png";
 import { Link } from "react-router-dom";
-
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function MainHeader() {
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
-      {/* ===== Main Header ===== */}
       <header className="main-header">
+
         <div className="container flex-2">
 
           {/* Logo */}
           <div className="logo">
             <img src={logo} alt="logo" />
-
             <span className="logo-text">MEDINOVA</span>
           </div>
 
-          {/* Menu */}
-          <nav className="menu">
-           
-            <Link to="Home">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/Service">Service</Link>
-            <Link to="/pricing">Pricing</Link>
+          {/* Mobile Button */}
+          <div
+            className="menu-btn"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            {showMenu ? <FaTimes /> : <FaBars />}
+          </div>
 
+          {/* Menu */}
+          <nav className={showMenu ? "menu active" : "menu"}>
+
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/service">Service</Link>
+            <Link to="/pricing">Pricing</Link>
 
             <div className="dropdown">
               <Link to="/">Pages ▾</Link>
+
               <div className="dropdown-menu">
                 <Link to="/bloggrid">BlogGrid</Link>
-                <Link to="/blogdatail">Blog Datails</Link>
-               
+                <Link to="/blogdatail">Blog Details</Link>
               </div>
             </div>
 
-            <Link to="contact">Contact</Link>
+            <Link to="/contact">Contact</Link>
+
           </nav>
 
         </div>
+
       </header>
     </>
   );
